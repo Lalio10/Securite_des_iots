72c26cc05683a318214682ec8e29375814fcf478
Abdellah <99265207+AbdellahWB@users.noreply.github.com>
2024-07-08 08:59:24 +0200

    Update Troisième code.py

--- Troisième code.py
+++ Troisième code.py
@@ -3,18 +3,34 @@ import paho.mqtt.client as mqtt
 import time
 
 try:
-   while(True):
-       message = "{'data' : 'bedead' ,'port' : 5, 'time' : 'immediately'}"
+    # Boucle infinie pour envoyer des messages MQTT en continu
+    while(True):
+        # Définition du message à envoyer
+        message = "{'data' : 'bedead' ,'port' : 5, 'time' : 'immediately'}"
 
-       client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-       client.username_pw_set("expemb", "Y8LyXK2QFE1D")
-       client.connect("192.168.1.28", 1883, 60)
-       time.sleep(1)
-       client.publish("in/F4C/80A5F5F5", message)
-       time.sleep(1)
-       client.disconnect()
+        # Création d'un client MQTT
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
-       print("Message published successfully.")
-       time.sleep(60)
+        # Configuration des identifiants de connexion
+        client.username_pw_set("expemb", "Y8LyXK2QFE1D")
+
+        # Connexion au broker MQTT
+        client.connect("192.168.1.28", 1883, 60)
+        time.sleep(1)
+
+        # Publication du message sur le topic spécifié
+        client.publish("in/F4C/80A5F5F5", message)
+        time.sleep(1)
+
+        # Déconnexion du broker MQTT
+        client.disconnect()
+
+        # Affichage d'un message de confirmation
+        print("Message published successfully.")
+        
+        # Attente de 60 secondes avant d'envoyer le prochain message
+        time.sleep(60)
+
 except:
-   pass
+    # En cas d'exception, le script passe simplement (ne fait rien)
+    pass
